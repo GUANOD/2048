8c2421fc3fcd5aff82a8266e94d0c8410407f82e
GUANOD <guano.carneiro@outlook.com>
2021-08-23 10:24:44 +0200

    swipe fonctionality

--- 2048.js
+++ 2048.js
@@ -22,9 +22,9 @@ let marked =[];
 let score = 0;
 scoreDiv.innerText = score;
 
-//number of cells
+// //number of cells
 
-let cells = 0;
+// let cells = 0;
 
 //start game
 
@@ -33,6 +33,53 @@ let start = false;
 startButton.addEventListener('click', startGame);
 restartButton.addEventListener('click', startGame);
 restartButton1.addEventListener('click', startGame);
+
+document.addEventListener('touchstart', handleTouchStart);        
+document.addEventListener('touchmove', handleTouchMove);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches              // browser API
+}     
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];                                      
+  xDown = firstTouch.clientX;                                      
+  yDown = firstTouch.clientY;                                      
+};  
+
+function handleTouchMove(evt){
+  if ( ! xDown || ! yDown ) {
+    return;
+}
+
+var xUp = evt.touches[0].clientX;                                    
+var yUp = evt.touches[0].clientY;
+
+var xDiff = xDown - xUp;
+var yDiff = yDown - yUp;
+
+if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+    if ( xDiff > 0 ) {
+        moveLeft();
+    } else {
+        moveRight();
+    }                       
+} else {
+    if ( yDiff > 0 ) {
+        moveUp(); 
+    } else { 
+        moveDown();
+    }                                                                 
+}
+/* reset values */
+xDown = null;
+yDown = null;   
+}
+
+
 
 function startGame(){
   clearBoard();
@@ -122,6 +169,7 @@ function moveLeft(){
             color.innerText = grid[i][j-g-1];
             color.classList.add(`c${grid[i][j-g-1]}`);
             color.classList.remove(`c${grid[i][j-g]}`);
+            
             grid[i][j-g] = 0;
             document.querySelector(`.top${i}.left${j-g}`).remove();
           }
